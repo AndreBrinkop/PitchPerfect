871a6f5e5e8251f6d5c615abb641f52a4e942f18
André Brinkop <andre@brinkop.de>
2016-10-23 14:28:28 +0200

    Added images for the start- and stop recording buttons. Added some basic logic to the start- and stop recording buttons. Added a UINavigationController and a basic second ViewController which will be used to play back the recorded messages.
    (Contains modifications from the "ViewController and Multiple Views" chapter of the course.)

--- PitchPerfect/ViewController.swift
+++ PitchPerfect/ViewController.swift
@@ -10,24 +10,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var recordingLabel: UILabel!
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBOutlet weak var recordButton: UIButton!
+    @IBOutlet weak var recordingLabel: UILabel!
+    @IBOutlet weak var stopRecordingButton: UIButton!
+    
+    override func viewWillAppear(_ animated: Bool) {
+        stopRecordingButton.isEnabled = false
     }
     
     @IBAction func recordAudio(_ sender: AnyObject) {
         print("record button pressed")
         recordingLabel.text = "Recording in progress"
+        stopRecordingButton.isEnabled = true
+        recordButton.isEnabled = false
     }
 
     @IBAction func stopRecording(_ sender: AnyObject) {
         print("stop recording button pressed")
+        recordingLabel.text = "Tap to Record"
+        stopRecordingButton.isEnabled = false
+        recordButton.isEnabled = true
     }
 }
 
